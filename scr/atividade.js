b6b8fe332cbe8fcb5e55517385e7fab024020390
Gabriel Assis <gabrielassissilva27@gmail.com>
2024-12-12 17:39:51 -0300

    commit inicial

---
+++ scr/atividade.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+function saudar(nome) {
+    return "Olá " + nome;
+}
+const resultadoMultiplicacao = multiplicar(3, 4);
+console.log(resultadoMultiplicacao);
+const saudacao = saudar("Maria");
+console.log(saudacao);
